7b8d25d18dd1e5689c113116a98922ec70b2d331
Aleksei Shamenev <coffius@gmail.com>
2016-01-28 00:21:48 +0300

    measure execution time

--- src/main/scala/io/koff/risk_ident/Main.scala
+++ src/main/scala/io/koff/risk_ident/Main.scala
@@ -4,10 +4,12 @@ import java.io.File
 
 object Main {
   def main(args: Array[String]) {
+    val startTime = System.currentTimeMillis()
     val result = OrderScheduler.readFileAndCalcMAT(new File("big_data.txt"))
+    val diff = System.currentTimeMillis() - startTime
     result match {
       case Left(err) => println(s"error: [$err]")
-      case Right(mat) => println(s"result: $mat")
+      case Right(mat) => println(s"result: $mat. time: $diff msec")
     }
   }
 }
